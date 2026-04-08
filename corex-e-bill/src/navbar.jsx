dd8260bc850914185b776af7a2feecdb8fabbeac
Bruxe-Dev <mnibeza23@gmail.com>
2026-04-08 20:30:53 +0200

    started Working on the navbar

---
+++ corex-e-bill/src/navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+    return (
+        <nav className="flex justify-between items-center py-6 px-10 bg-[#0a0f1a] border-b border-gray-800 sticky top-0 z-50">
+            <div className="flex items-center gap-2"></div>i
+        </nav>
+    )
+}
